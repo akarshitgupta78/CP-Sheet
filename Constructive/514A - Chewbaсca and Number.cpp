30e72ecdb465613f5610c781246a42dba9ff8ff9
Akarshit Gupta <akarshitg7800@gmail.com>
2026-09-07 10:51:15 +0530

    514A - Chewbaсca and Number.cpp

---
+++ Constructive/514A - Chewbaсca and Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    string s;
+    cin >> s;
+    int n= s.size();
+    for (int i = 0;i<n;i++) 
+    {
+        int digit = s[i]-'0',rem =9-digit;
+        if (i == 0 && rem == 0)
+            continue;
+        if (rem< digit) 
+            s[i] = (rem + '0');
+    }
+    cout << s;
+    return 0;
+}
